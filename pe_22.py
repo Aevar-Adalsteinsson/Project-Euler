4f756b299eaf96bd1ed30b43e2df72b61392c47d
Aevar-Adalsteinsson <aevaradal@gmail.com>
2025-01-24 20:32:39 +0000

    22,42

--- pe_22.py
+++ pe_22.py
@@ -4,6 +4,25 @@ import math
 import itertools
 import pehelperfunctions
 
+def pe_22():
+    f = open("pe_22_names.txt", "r")
+
+    names = f.read()
+    names = names.split("\",\"")
+    n = len(names)
+    names[0] = names[0][1:]
+    names[n-1] = names[n-1][:-1]
+    names.sort()
+    
+    names_score = 0
+    for i in np.arange(len(names)):
+        name = names[i]
+        score = 0
+        for j in np.arange(len(name)):
+            score += ord(name[j])-64
+        names_score += (i+1)*score
+    return(names_score)
+
 
 
 if __name__ == '__main__':
--- pe_42.py
+++ pe_42.py
@@ -4,6 +4,28 @@ import math
 import itertools
 import pehelperfunctions
 
+def pe_42():
+    f = open("pe_42_words.txt", "r")
+    words = f.read()
+    words = words.split(",")
+    words = [word[1:-1] for word in words]
+
+    max_tri = 0
+    max_tri_n = 0
+    tri = []
+    tri_n = 0
+    for word in words:
+        val = 0
+        for c in word:
+            val += ord(c)-64
+        while val > max_tri:
+            max_tri_n += 1
+            max_tri = max_tri_n*(max_tri_n+1)//2
+            tri.append(max_tri)
+        if val in tri:#binary search or turn tri into set for more efficiency
+            tri_n += 1
+    return(tri_n)
+
 
 
 if __name__ == '__main__':
